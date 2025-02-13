f05dc9971017417d3eda9bd30f46d20a648c608b
Jakku Night <R101g117l117s@gmail.com>
2025-02-13 18:15:01 -0300

    Add more dev tools. + Patch font with NerdFont patcher.

--- default.nix
+++ default.nix
@@ -1,7 +1,12 @@
 { pkgs, inputs, ... }: pkgs.stdenv.mkDerivation {
   name = "GenshinImpact";
   src = inputs.hywenhei-extended-font;
-  dontBuild = true;
+  buildInputs = [
+    pkgs.nerd-font-patcher
+  ];
+  buildPhase = ''
+    nerd-font-patcher --name "Genshin-Impact" $src/*.ttf -out .
+  '';
   installPhase = ''
     mkdir -p $out/GenshinImpact
     cp -r $src/* $out/GenshinImpact
---
+++ devenv.nix
@@ -0,0 +1,42 @@
+{ pkgs, lib, config, inputs, ... }:
+
+{
+  # https://devenv.sh/basics/
+  env = {};
+
+  # https://devenv.sh/packages/
+  packages = [
+    pkgs.git
+    pkgs.nerd-font-patcher
+  ];
+
+  # https://devenv.sh/languages/
+  # languages.rust.enable = true;
+
+  # https://devenv.sh/processes/
+  # processes.cargo-watch.exec = "cargo-watch";
+
+  # https://devenv.sh/services/
+  # services.postgres.enable = true;
+
+  # https://devenv.sh/scripts/
+  scripts = {};
+
+  enterShell = ''
+  '';
+
+  # https://devenv.sh/tasks/
+  # tasks = {
+  #   "myproj:setup".exec = "mytool build";
+  #   "devenv:enterShell".after = [ "myproj:setup" ];
+  # };
+
+  # https://devenv.sh/tests/
+  enterTest = ''
+  '';
+
+  # https://devenv.sh/git-hooks/
+  # git-hooks.hooks.shellcheck.enable = true;
+
+  # See full reference at https://devenv.sh/reference/options/
+}
