18fce5b0870f8fbab1e4ab5fabadc32af9bd41d3
Jakku Night <R101g117l117s@gmail.com>
2025-02-13 18:56:56 -0300

    Patch font with NerdFont Icons.

--- default.nix
+++ default.nix
@@ -1,14 +1,13 @@
 { pkgs, inputs, ... }: pkgs.stdenv.mkDerivation {
   name = "GenshinImpact";
   src = inputs.hywenhei-extended-font;
-  buildInputs = [
+  nativeBuildInputs = [
     pkgs.nerd-font-patcher
   ];
-  buildPhase = ''
-    nerd-font-patcher --name "Genshin-Impact" $src/*.ttf -out .
-  '';
   installPhase = ''
-    mkdir -p $out/GenshinImpact
-    cp -r $src/* $out/GenshinImpact
+    mkdir -p $out
+    cp -r $src/* $out
+    nerd-font-patcher --name "Genshin-Impact" $out/*.ttf -out $out
+    rm $out/"HYWenHei Extended.ttf"
   '';
 }
